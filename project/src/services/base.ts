d96900de86b0097342fde299181a76b20d98ee95
darknessxk <me@darknessxk.com>
2020-12-02 19:36:17 -0300

    Fix wrong protocol for baseURL
    Add some cors issue solution

--- project/src/services/base.ts
+++ project/src/services/base.ts
@@ -3,7 +3,15 @@ import axios from 'axios';
 const dev = process.env.NODE_ENV !== 'production';
 
 const axiosObject = axios.create({
-    baseURL: dev ? 'https://localhost:3001/api/' : 'api/'
+    baseURL: dev ? 'http://localhost:3001/api/' : 'api/'
+})
+
+axiosObject.interceptors.request.use(req => {
+    req.headers['Access-Control-Allow-Origin'] = '*';
+    req.headers['Access-Control-Allow-Headers'] = 'Authorization';
+    req.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, PUT, PATCH, DELETE';
+
+    return req;
 })
 
 export default axiosObject;
