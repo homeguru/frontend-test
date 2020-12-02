d94ebfef03175ad7be6ee6a22f5678fff1a86c72
darknessxk <me@darknessxk.com>
2020-12-02 15:43:55 -0300

    Initial Project

---
+++ project/src/reportWebVitals.ts
@@ -0,0 +1,14 @@
+import { ReportHandler } from 'web-vitals';
+
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+  if (!onPerfEntry) return;
+  import('web-vitals').then(({getCLS, getFID, getFCP, getLCP, getTTFB}) => {
+    getCLS(onPerfEntry);
+    getFID(onPerfEntry);
+    getFCP(onPerfEntry);
+    getLCP(onPerfEntry);
+    getTTFB(onPerfEntry);
+  });
+};
+
+export default reportWebVitals;
