d9741efe560fd9fabb88c8c6b7004267bad59dd4
darknessxk <me@darknessxk.com>
2020-12-03 18:03:20 -0300

    Updated Comic component

--- project/src/components/Comic/index.tsx
+++ project/src/components/Comic/index.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
-import { Container } from './styles';
-import Loading from '../Loading';
+import {ComicCover, Container, TextObject} from './styles';
 import { Thumbnail } from "../../interfaces/thumbnail";
-import LazyComponent from "../LazyComponent";
 import moment from "moment";
+import {GetImage} from "../../utils/marvel.image.parser";
+import { useDispatch } from 'react-redux';
+
+import { setComic } from '../../redux/app';
+import LazyImage from "../LazyImage";
 
 interface ComicProps {
     Id: number;
@@ -13,51 +16,16 @@ interface ComicProps {
 }
 
 const Comic: React.FC<Readonly<ComicProps>> = ({Id, Date, Name, Thumbnail}) => {
-    return <Container>
-        <LazyComponent<object> key={Id} fetch={() => {
-            return new Promise(resolve => {
-                setTimeout(() => {
-                    resolve(<>
-                        <LazyComponent<React.ImgHTMLAttributes<HTMLImageElement>> fetch={(props) => {
-                            return new Promise(resolve => {
-                                const image = new Image();
-                                const imageProps = props as unknown as React.ImgHTMLAttributes<HTMLImageElement>;
+    const imageFile = GetImage(Thumbnail, "portrait_incredible");
+    const dispatch = useDispatch();
+    const onClickHandler = () => {
+        dispatch(setComic(Id));
+    };
 
-                                image.addEventListener('load', () => {
-                                    resolve(<img src={image.src} alt={imageProps.alt} {...imageProps} />)
-                                })
-
-                                image.addEventListener('error', () => {
-                                    resolve(
-                                        <Loading
-                                            message="Unable to load image..."
-                                            position="relative"
-                                            minHeight={256}
-                                            minWidth={256}
-                                            shadow
-                                        />)
-                                });
-
-                                image.src = imageProps.src!;
-                            });
-                        }} loaderComponent={
-                            <Loading
-                                message="Image is loading"
-                                position="relative"
-                                minHeight={256}
-                                minWidth={256}
-                                shadow
-                            />
-                        } {...{
-                            width: "128",
-                            src: "https://via.placeholder.com/2048x2048"
-                        }} />
-                        <p>Title: {Name}</p>
-                        <p>Release Date: {moment(Date).format('d / M / y')}</p>
-                    </>);
-                }, 1250)
-            });
-        }} loaderComponent={<Loading message="Comic is loading" position="relative" />} />
+    return <Container key={Id} onClick={onClickHandler}>
+        <LazyImage imageFile={imageFile} baseComponent={<ComicCover />} />
+        <TextObject>{Name}</TextObject>
+        <TextObject>Release Date: {moment(Date).format('DD / MM / YYYY')}</TextObject>
     </Container>;
 }
 
--- project/src/components/Comic/styles.tsx
+++ project/src/components/Comic/styles.tsx
@@ -1,3 +1,29 @@
 import styled from 'styled-components';
+import {Colors} from "../../theme";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 8px;
+  align-items: center;
+  height: 500px;
+  max-width: 256px;
+  background: linear-gradient(180deg, ${Colors["3"]}8F, ${Colors["3"]}8F, transparent);
+`;
+
+export const ComicCover = styled.img`
+  width: 256px;
+  z-index: 1;
+  background: ${Colors["3"]}8F;
+`;
+
+export const TextObject = styled.p`
+  & {
+    padding: 4px;
+  }
+  
+  font-weight: bold;
+  
+  word-break: break-word;
+  text-align: center;
+`;
