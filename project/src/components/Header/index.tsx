b6d2904bf9cf531fe26cb3edc9675dd23126f805
darknessxk <me@darknessxk.com>
2020-12-03 11:58:40 -0300

    Updated header Added a few styles

--- project/src/components/Header/index.tsx
+++ project/src/components/Header/index.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
+import { Container } from "./styles";
 
-const Header: React.FC = () => {
-    return null;
+const Header: React.FC = ({children}) => {
+    return <Container>
+        {children}
+    </Container>;
 }
 
 export default Header;
--- project/src/components/Header/styles.tsx
+++ project/src/components/Header/styles.tsx
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
+import { Colors } from "../../theme";
 
-const Container = styled.div``;
-
-export default Container;
+export const Container = styled.header`
+  display: flex;
+  background: ${Colors["2"]};
+  min-height: 48px;
+  color: ${Colors["4"]};
+  justify-content: center;
+  align-items: center;
+  font-weight: 700;
+  font-size: 16pt;
+`;
