1842725122ce96473a2491405653163460ea7747
darknessxk <me@darknessxk.com>
2020-12-03 11:59:13 -0300

    Updated main

--- project/src/pages/Main/index.tsx
+++ project/src/pages/Main/index.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react';
 import {Character} from "../../interfaces/character";
+import { Container } from './styles'
+import Comic from "../../components/Comic";
+import Layout from "../Layout";
 
 interface MainProps {
     character: Character;
 }
 
 const Main: React.FC<MainProps> = ({character}) => {
-    return <div>
-        Selected Character: {character.name}
-    </div>;
+    return <Layout>
+        <Container>
+            <Comic Id={1} Date={new Date()} Name={"Test"} Thumbnail={{
+                extension: "jpeg",
+                path: "https://via.placeholder.com/128x128"
+            }} />
+        </Container>
+    </Layout>;
 }
 
 export default Main;
