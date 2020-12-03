6c239cadce878272267b8d5b8aefb3cda610b06c
darknessxk <me@darknessxk.com>
2020-12-02 22:38:12 -0300

    Created based component for comic

---
+++ project/src/components/Comic/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Container } from './styles';
+
+const Comic: React.FC = () => {
+    return <Container>
+        Comic...
+    </Container>;
+}
+
+export default Comic;
